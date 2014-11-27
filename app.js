1a0ad3a84fe1bb736e8eaa48702bbd85d114458d
ks-s-a <ksenofontovsa@yandex.ru>
2014-11-27 13:35:48 +0300

    #8 Remade templates files. Add logger (winston). Add group handling for extraction list. Create a openWeather condition table (not use yet). Prepare for add some new sources.

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 var Step = require('step');
 var packageData = require('./lib/packageData/packageData.js');
 var app = require('express')();
+var winston = require('winston');
 
 app.get('/', function(req, res) {
     Step(
         function(){
-            packageData(this);
+            winston.info('Connection established!');
+
+            packageData('London', this);
         },
         function (err, data) {
             if (err) throw err;
-            res.render('layout.jade', {appName: 'WeatherApp', data: data});
+
+            winston.info('Receive common weather data: ', data);
+
+            res.render('index.jade', {
+                appName: 'WeatherApp',
+                data: data
+            });
+
+            res.end();
+
+            winston.info('Connection closed successfully!');
         }
     );
+
+
 });
 
 var server = app.listen(2345, function() {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log('Start my app at http://%s:%s', host, port);
+    winston.info('Start my app at http://%s:%s', host, port);
 });
